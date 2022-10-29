7a5624729c8d6366e26b3f565e28dec269ed11d1
Usman Tahir <65866570+UsmanTahir7@users.noreply.github.com>
2022-10-29 02:41:30 -0400

    complete

--- app.js
+++ app.js
@@ -30,8 +30,6 @@ mongoose.connect(process.env.MONGO_DB_URL);
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
-    googleId: String,
-    facebookId: String,
     secret: String
 });
 
