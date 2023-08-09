996b55b9a96f2e60e7a9c409d269f5df3ca51d15
Usman Tahir <usman.tahi7@gmail.com>
2023-08-08 23:16:27 -0400

    dassa

--- app.js
+++ app.js
@@ -189,3 +189,11 @@ app.post(
   }
 );
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("Server started on port 3000.");
+});
